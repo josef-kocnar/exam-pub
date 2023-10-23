8429a4fb357c46cabd0d255117b483e5af294ebe
Josef Kočnar <kocnar.jos@gmail.com>
2023-10-24 00:04:14 +0200

    feat(): add product class

---
+++ src/main/java/com/example/exampub/models/Product.java
@@ -0,0 +1,22 @@
+package com.example.exampub.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String productName;
+    private double price;
+    private boolean isForAdult;
+}
