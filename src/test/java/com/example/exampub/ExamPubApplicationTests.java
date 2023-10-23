6adf2f99a4bbac7e165d012ad7a43ab25df3029d
Josef Kočnar <kocnar.jos@gmail.com>
2023-10-23 19:06:00 +0200

    project initialization

---
+++ src/test/java/com/example/exampub/ExamPubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exampub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
